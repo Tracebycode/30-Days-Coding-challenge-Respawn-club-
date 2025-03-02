1c5951ab5307836af37733711594baabf1c0ccf6
Abhishek Barik <abhishekbarik974@gmail.com>
2025-03-02 23:31:27 +0530

    Day 2 Coding challenge Compelete

--- Day 2/day2.cpp
+++ Day 2/day2.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 
 int occurance(int array[], int size, int x) {
-    int count = 0;  // Initialize count to 0
+    int count = 0;  
     for (int i = 0; i < size; i++) {
-        if (array[i] == x) {  // Compare element, not index
+        if (array[i] == x) {  
             count++;
         }
     }
-    return count;  // Return count after the loop finishes
-}
+    return count;  
 
 int main() {
     int array[] = {10, 24, 19, 26, 27, 31, 33, 35, 42, 44};
-    int size = sizeof(array) / sizeof(array[0]);  // Correctly calculate size
-
+    int size = sizeof(array) / sizeof(array[0]);  
     cout << "Enter the number whose occurrence is to be found: ";
     int x;
     cin >> x;
